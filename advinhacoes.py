2b63eac941833138da475aedca61f6c71148af8f
Gabriel da Silva <gabriel@biopetroambiental.com.br>
2022-09-08 15:12:06 -0300

    Initial commit

---
+++ advinhacoes.py
@@ -0,0 +1,37 @@
+import random
+
+
+print("Olá Mundo !")
+print("Bem vindos no jogo de advinha !")
+print("************************************")
+
+numero_secreto = round(random.randrange(1, 101))
+total_de_tentativas = 5
+
+print(numero_secreto)
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número entre 1 e 100: ")
+    print("Você digitou", chute_str)
+    chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100 !")
+        continue
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if acertou:
+        print("Você acertou!")
+        break
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor que o número secreto.")
+    rodada = rodada + 1
+
+    print("Fim de Jogo.")
