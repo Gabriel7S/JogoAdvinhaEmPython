19d111e0bc02832776c7eeded463e2c40adcaeff
Gabriel <Gabriel710Silva@gmail.com>
2022-09-08 16:11:13 -0300

    Adicionado sistemas de pontuação ao acertar.

--- advinhacoes.py
+++ advinhacoes.py
@@ -2,13 +2,28 @@ import random
 
 
 print("Olá Mundo !")
-print("Bem vindos no jogo de advinha !")
+print("************************************")
+print("Bem vindos no jogo de advinhação!")
 print("************************************")
 
 numero_secreto = round(random.randrange(1, 101))
-total_de_tentativas = 5
+total_de_tentativas = 0
+pontos = 1000
 
-print(numero_secreto)
+
+print("Escolha um nível de dificuldade: ")
+print("(1) Fácil (2) Médio (3) Difícil")
+print("************************************")
+nivel = int(input("Defina um nível: "))
+
+print("************************************")
+
+if (nivel==1):
+    total_de_tentativas = 20
+elif(nivel==2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -16,7 +31,7 @@ for rodada in range(1, total_de_tentativas + 1):
     print("Você digitou", chute_str)
     chute = int(chute_str)
 
-    if chute < 1 or chute > 100:
+    if(chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100 !")
         continue
 
@@ -24,14 +39,15 @@ for rodada in range(1, total_de_tentativas + 1):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if acertou:
-        print("Você acertou!")
+    if (acertou):
+        print("Você acertou e fez {} pontos !".format(pontos))
         break
     else:
-        if maior:
+        if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
-        elif menor:
+        elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
-    rodada = rodada + 1
+    pontos_perdidos = abs(numero_secreto - chute)
+    pontos = pontos - pontos_perdidos
 
-    print("Fim de Jogo.")
+print("Fim de Jogo.")
